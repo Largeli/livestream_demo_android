23cd6a33978bf456881074af894efd037c747341
lijianjun <1020942021@qq.com>
2017-03-06 14:32:59 +0800

    我的钱包界面修改，添加loading

--- app/src/main/java/cn/ucai/live/ui/activity/ChangeActivity.java
+++ app/src/main/java/cn/ucai/live/ui/activity/ChangeActivity.java
@@ -2,16 +2,26 @@ package cn.ucai.live.ui.activity;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import cn.ucai.live.R;
 
 /**
  * Created by Administrator on 2017/3/6 0006.
  */
-public class ChangeActivity extends BaseActivity{
+public class ChangeActivity extends BaseActivity {
+    @BindView(R.id.tv_change_balance)
+    TextView tvChangeBalance;
+    @BindView(R.id.target_layout)
+    LinearLayout targetLayout;
+
     @Override
     public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
         setContentView(R.layout.activity_change);
+        ButterKnife.bind(this);
     }
 }
